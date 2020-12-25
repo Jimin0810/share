aadecedf849d19cdae1f1ec78a32c703a49f83b2
jimin <624012124@qq.com>
2020-12-25 16:43:17 +0800

    feat: 1.初始化首页及样式 2.添加bootstrap样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import '/@/assets/style/bootstrap.less'
 import '/@/assets/style/index.less'
 
 createApp(App).use(router).use(store).mount('#app')
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,12 +1,24 @@
 import { ref, watchEffect, defineComponent } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute, useRouter } from 'vue-router'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
 export default defineComponent({
   setup() {
     const { state, commit } = useStore()
     const router = useRouter()
     const route = useRoute()
-    const files = new Array(10).fill(0).map((_, idx: number) => idx + 1)
+    const files = new Array(3).fill(0).map((_, idx: number) => {
+      return {
+        id: idx,
+        name: '文件名',
+        size: 1024 * (idx + 1),
+        type: (idx + 1) % 3,
+        thumb: 'xxx',
+        created_at: new Date().toLocaleDateString(),
+        updated_at: new Date().toLocaleDateString(),
+      }
+    })
     let currentPath: string
 
     const handleClick = (url: string) => {
@@ -18,15 +30,29 @@ export default defineComponent({
     })
 
     return () => (
-      <div class="file">
-        <ul>
+      <div class="wrap">
+        <Header />
+        <div class="list-header">
+          <div class="col-md-7 col-sm-6 col-xs-8">文件名</div>
+          <div class="col-md-2 col-sm-2 col-xs-4 text-right">大小</div>
+          <div class="col-md-3 col-sm-4 hidden-xs text-right">修改时间</div>
+        </div>
+        <ul class="list">
           {files.map((i) => (
-            <li onClick={() => handleClick(i)}>
-              {currentPath}
-              {i}/
+            <li onClick={() => handleClick(i.id + '')}>
+              <a class="clearfix" href="javascript:void(0)" target="">
+                <div class="row">
+                  <span class="file-name col-md-7 col-sm-6 col-xs-8">
+                    <i class={{ ic: true, 'ic-folder': true }}></i> {currentPath}
+                  </span>
+                  <span class="file-size col-md-2 col-sm-2 col-xs-4 text-right">{i.size} GB</span>
+                  <span class="file-modified col-md-3 col-sm-4 hidden-xs text-right">{i.created_at}</span>
+                </div>
+              </a>
             </li>
           ))}
         </ul>
+        <Footer />
       </div>
     )
   },
